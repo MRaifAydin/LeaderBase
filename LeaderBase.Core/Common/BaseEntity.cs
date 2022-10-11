a14c80d4129d3d17635f709377adf427ae2c6938
MRaifAydin <mert5708@gmail.com>
2022-10-11 12:11:25 +0300

    Added Core and Dto classes.

---
+++ LeaderBase.Core/Common/BaseEntity.cs
@@ -0,0 +1,38 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeaderBase.Core.Common
+{
+    public class BaseEntity
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        [BsonId]
+        [BsonElement(Order = 0)]
+        public string Id { get; set; }
+
+        [BsonRepresentation(BsonType.DateTime)]
+        [BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
+        [BsonElement(Order = 1001)]
+        public DateTime? CreatedAt { get; set; }
+
+        [BsonRepresentation(BsonType.DateTime)]
+        [BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
+        [BsonElement(Order = 1002)]
+        public DateTime? LastModifiedAt { get; set; }
+
+        [BsonElement(Order = 1003)]
+        public long? CreatedBy { get; set; }
+
+        [BsonElement(Order = 1004)]
+        public long? LastModifiedBy { get; set; }
+
+        [BsonElement(Order = 1005)]
+        [BsonRepresentation(BsonType.Boolean)]
+        public bool IsDeleted { get; set; }
+    }
+}
---
+++ LeaderBase.Dto/Persons/PersonDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeaderBase.DTO.Persons
+{
+    public class PersonDto
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string PlaceOfBirth { get; set; }
+
+        public string Nationality { get; set; }
+    }
+}
