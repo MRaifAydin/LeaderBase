2099b62dd4bfa936a567183254cb5640dc215b47
MRaifAydin <mert5708@gmail.com>
2022-10-11 16:20:36 +0300

    Added base repository.

---
+++ LeaderBase.Repository/Common/IBaseRepository.cs
@@ -0,0 +1,29 @@
+﻿using LeaderBase.Core.Common;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeaderBase.Repository.Common
+{
+    public interface IBaseRepository<TSource> where TSource : BaseEntity
+    {
+        IMongoQueryable<TSource> GetAll();
+        IMongoQueryable<TSource> GetAllWithDeleted();
+        IMongoQueryable<TSource> Where(Expression<Func<TSource, bool>> predicate);
+        IMongoQueryable<TSource> GetById(string id) => Where(x => x.Id == id);
+        IMongoQueryable<TSource> GetByIdWithDeleted(string id);
+
+        Task InsertOne(TSource entity);
+        Task InsertMany(IEnumerable<TSource> entity);
+
+        Task<ReplaceOneResult> UpsertAsync(TSource entity);
+
+        Task<UpdateResult> DeleteAsync(string id);
+        Task<UpdateResult> DeleteAsync(Expression<Func<TSource, bool>> predicate);
+    }
+}
