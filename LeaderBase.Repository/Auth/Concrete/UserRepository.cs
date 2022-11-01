77ea3fb57c966840944dd2a01c415d976ccee7b3
MRaifAydin <mert5708@gmail.com>
2022-11-01 10:46:29 +0300

    Added User Repository.

---
+++ LeaderBase.Repository/Auth/Concrete/UserRepository.cs
@@ -0,0 +1,19 @@
+﻿using LeaderBase.Core.Auth.Entities;
+using LeaderBase.DataAccess;
+using LeaderBase.Repository.Auth.Abstract;
+using LeaderBase.Repository.Auth.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeaderBase.Repository.Auth.Concrete
+{
+    public class UserRepository : BaseRepository<User>, IUserRepository
+    {
+        public UserRepository(UserDbContext context) : base(context)
+        {
+        }
+    }
+}
