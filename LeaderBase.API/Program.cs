1d4c45c4f04744a0b7ad3a9b3514da75356fef2a
MRaifAydin <mert5708@gmail.com>
2022-11-01 10:34:15 +0300

    Revert "Added User entity and created database."
    This reverts commit 6e4cfefd07a6cb9537c903c451aec6271d4fc3f5.

--- LeaderBase.API/Program.cs
+++ LeaderBase.API/Program.cs
@@ -6,13 +6,10 @@ using LeaderBase.Business.Concrete;
 using LeaderBase.Business.DependecyResolvers.Autofac;
 using LeaderBase.Conversion;
 using LeaderBase.Core.Common;
-using LeaderBase.DataAccess;
 using LeaderBase.Repository.Abstract;
 using LeaderBase.Repository.Concrete;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection.Extensions;
-using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -31,8 +28,6 @@ builder.Services.Configure<LeaderBaseDbSettings>
 ConversionDIModule.Inject(builder.Services, builder.Configuration);
 
 builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
-
-builder.Services.AddDbContext<UserDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSQL")));
 
 builder.Host.ConfigureContainer<ContainerBuilder>(builder =>
 {
